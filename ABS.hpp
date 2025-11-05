79988515e4ea04b9e1e30660c3dace2bac9f71c3
Zlata-Kovrigina <zkovrigina@ufl.edu>
2025-11-05 22:06:36 +0000

    v3

--- ABS.hpp
+++ ABS.hpp
@@ -11,29 +11,98 @@ template<typename T>
 class ABS : public StackInterface<T> {
 public:
     // Big 5 + Parameterized Constructor
-    ABS() : capacity_(1), size_(0), {}
-    explicit ABS(const size_t capacity);
-    ABS(const ABS& other);
-    ABS& operator=(const ABS& rhs);
-    ABS(ABS&& other) noexcept;
-    ABS& operator=(ABS&& rhs) noexcept;
-    ~ABS() noexcept override;
+    ABS() : capacity_(1), curr_size_(0), {
+        array_ = new T[capacity_];
+    }
+
+    explicit ABS(const size_t capacity) : capacity_(capacity), curr_size_(0) {
+        array_ = new T[capacity_];
+    }
+
+    ABS(const ABS& other) : capacity_(other.capacity_), curr_size_(other.curr_size_) {
+        array_ = new T[capacity_];
+        for (size_t i = 0; i < curr_size_; i++) {
+            array_[i] = other.array_[i];
+        }
+    }
+
+    ABS& operator=(const ABS& rhs) {
+        if (this = &rhs) {
+            return *this;
+        }
+        delete[] array_;
+        capacity_ = rhs.capacity_;
+        curr_size_ = rhs.curr_size_;
+        array_ = new T[capacity_];
+         for (size_t i = 0; i < curr_size_; i++) {
+            array_[i] = rhs.array_[i];
+        }
+        return *this;
+    }
+
+    ABS(ABS&& other) noexcept : capacity_(other.capacity_), curr_size_(other.curr_size_){
+        array_ = other.array_;
+        other.array_ = nullptr;
+        other.capacity_ = 0;
+        other.curr_size_ = 0;
+    }
+
+    ABS& operator=(ABS&& rhs) noexcept {
+        if (this = &rhs) {
+            return *this;
+        }
+        delete[] array_;
+        capacity_ = rhs.capacity_;
+        curr_size_ = rhs.curr_size_;
+        array_ = rhs.array_;
+        rhs.array_ = nullptr;
+        rhs.curr_size_ = 0;
+        rhs.capacity_ = 0;
+        return *this;
+    }
+
+    ~ABS() noexcept override {
+        delete[] array_;
+    }
 
     // Get the number of items in the ABS
-    [[nodiscard]] size_t getSize() const noexcept override;
+    [[nodiscard]] size_t getSize() const noexcept override {
+        return curr_size_;
+    }
 
     // Get the max size of the ABS
-    [[nodiscard]] size_t getMaxCapacity() const noexcept;
+    [[nodiscard]] size_t getMaxCapacity() const noexcept {
+        return capacity_;
+    }
 
     // Return underlying data for the stack
-    [[nodiscard]] T* getData() const noexcept;
+    [[nodiscard]] T* getData() const noexcept {
+        return array_;
+    }
 
     // Push item onto the stack
-    void push(const T& data) override;
+    void push(const T& data) override {
+        if (curr_size_ = capacity_) {
+            size_t newCapacity_ = capacity_ * scale_factor_;
+            newArray_ = new T[newCap];
+            for (size_t i = 0; i < curr_size_; i++) {
+                newArray_[i] = array_[i];
+            }
+            delete[] array_;
+            array_ = newArray_;
+            capacity_ = newCapacity_;
+        }
+        array_[curr_size_++] = data;
 
-    T peek() const override;
+    }
 
-    T pop() override;
+    T peek() const override {
+        return array_[curr_size_ - 1];
+    }
+
+    T pop() override {
+        return array_[size_ - 1]
+    }
 
 private:
     size_t capacity_;
