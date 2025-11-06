d7d929f45108d5e3b655c4416379ad5772777ecc
Zlata-Kovrigina <zkovrigina@ufl.edu>
2025-11-06 20:30:58 +0000

    v12

--- Interfaces.hpp
+++ Interfaces.hpp
@@ -6,30 +6,30 @@
 
 template <typename T>
 class StackInterface {
-    void push(const T& item);
-    T pop();
-    T peek() const;
-    std::size_t getSize() const noexcept;
+    virtual void push(const T& item) = 0;
+    virtual T pop() = 0;
+    virtual T peek() const = 0;
+    virtual std::size_t getSize() const noexcept = 0;
 };
 
 
 template <typename T>
 class QueueInterface {
-    void enqueue(const T& item);
-    T dequeue();
-    T peek() const {};
-    std::size_t getSize() const noexcept;
+    virtual void enqueue(const T& item) = 0;
+    virtual T dequeue() = 0;
+    virtual T peek() const {} = 0;
+    virtual std::size_t getSize() const noexcept = 0;
 };
 
 
 template <typename T>
 class DequeInterface {
-    void pushFront(const T& item);
-    void pushBack(const T& item);
-    T popFront();
-    T popBack();
-    const T& front() const;
-    const T& back() const;
-    std::size_t getSize() const noexcept;
+    virtual void pushFront(const T& item) = 0;
+    virtual void pushBack(const T& item) = 0;
+    virtual T popFront() = 0;
+    virtual T popBack() = 0;
+    virtual const T& front() const = 0;
+    virtual const T& back() const = 0;
+    virtual std::size_t getSize() const noexcept = 0;
 };
 
