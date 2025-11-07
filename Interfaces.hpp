3238727872524461b8bd3e627c31f247a42c2f40
Zlata-Kovrigina <zkovrigina@ufl.edu>
2025-11-07 05:29:22 +0000

    v26

--- Interfaces.hpp
+++ Interfaces.hpp
@@ -8,7 +8,7 @@ template <typename T>
 class StackInterface {
     virtual void push(const T& item) = 0;
     virtual T pop() = 0;
-    virtual const T& peek() const = 0;
+    virtual T peek() const = 0;
     virtual std::size_t getSize() const noexcept = 0;
 };
 
@@ -17,7 +17,7 @@ template <typename T>
 class QueueInterface {
     virtual void enqueue(const T& item) = 0;
     virtual T dequeue() = 0;
-    virtual const T& peek() const = 0;
+    virtual T peek() const = 0;
     virtual std::size_t getSize() const noexcept = 0;
 };
 
